de30f49513835e7d65edc44d1709cb7e719230f9
Ramesh <rameshbabu9889@gmail.com>
2019-06-21 02:18:26 -0500

    resume

---
+++ src/actions/shared.js
@@ -0,0 +1,12 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { receiveTweets } from '../actions/tweets'
+
+export function handleInitialData(){
+    return (dispatch) => {
+        return getInitialData().then(({ users, tweets }) => {
+            dispatch(receiveUsers(users))
+            dispatch(receiveTweets(tweets))
+        })
+    }
+}
