d312dd2943d6c82b87df7d08bc6dc9d5606c35dd
Ramesh <rameshbabu9889@gmail.com>
2019-06-22 03:53:44 -0500

    progess1

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,16 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
+import { setAuthedUserId } from '../actions/authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData(){
     return (dispatch) => {
         return getInitialData().then(({ users, tweets }) => {
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
+            dispatch(receiveTweets(AUTHED_ID))
         })
     }
 }
