3846cf4b9390db8cba3b61eb8c48d8f66e38b6eb
Ramesh <rameshbabu9889@gmail.com>
2019-07-08 01:15:58 -0500

    Lesson-12-Complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
+import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
+import Tweet from './Tweet';
 
 class App extends Component {
   componentDidMount(){
@@ -13,7 +16,7 @@ class App extends Component {
       <div>
         <LoadingBar/>
         {this.props.loading ? null:
-          <Dashboard/>
+          <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}}/>
         }
       </div>
     )
